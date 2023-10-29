ded71d15c29c618789597589a010344106a7cc6f
liammx <liammx03@gmial.com>
2023-10-29 08:39:57 +0500

    jsdom

---
+++ main.js
@@ -0,0 +1,22 @@
+// DOM document object module
+// let h1 = document.getElementsByTagName('h1')
+// let h1class = document.getElemetnByClassname('genrix')
+// let h1id = document.getElementById('salom')
+
+// let a = document.querySelector('#salom')
+// let b = document.querySelector('.genrix')
+// let c = document.querySelector('h1')
+
+// let f = document.querySelectorAll('h1')
+
+// console.log(f);
+// console.log(h1class);
+
+let a = document.querySelector('.text')
+let box = document.querySelector('.box')
+
+a.innerHTML = '<span> hi </span>'
+
+box.style.width = '200px'
+box.style.height = '200px'
+box.style.backgroundColor = '#000'
